da5180efa3ba8c3940f1099d80092615850b45a3
YouthInXn <yuanjia.fan@outlook.com>
2019-03-07 18:23:05 +0800

    修改用户界面

--- MyBlog/src/router/Author/components/author.js
+++ MyBlog/src/router/Author/components/author.js
@@ -1,62 +1,134 @@
-
 import React from 'react'
-import './authorStyle/style.css'
+import '../style.css'
 import {
-  Grid, Avatar, List,
-  ListItem, ListItemIcon, ListItemText,
+  Avatar, Paper, Chip, Typography, Fab, Tooltip
 } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 import projectConfig from '../../../../myBlog.config'
+import {
+  AlarmOn, ChatBubbleOutline, AccountCircleOutlined, ShareOutlined
+} from '@material-ui/icons'
+import { Link, Route } from 'react-router-dom'
+import Likes from './Likes'
+// author下的公用组件
+const iconStyle = { width:'27px', height:'27px' }
 
 const styles = theme => ({
-  root:{
-    flewGrow:1
+  navContainer: {
+    position:'relative',
+    top:30,
+    right:-80,
+    width:'100px',
+    height:'400px',
+    float: 'right'
+  },
+  fabButton: {
+    margin: theme.spacing.unit,
+  },
+  currentMenu:{
+    margin: theme.spacing.unit,
+    backgroundColor:'#AFB1AF',
   },
   avatar:{
     width:100,
     height:100,
-    margin:'10px auto'
-  }
+    position:'relative',
+    left:'50px',
+    top:'-50px'
+  },
+  paper:{
+    width: '70%',
+    minHeight:'900px',
+    margin:'10% auto 5% auto',
+    paddingBottom:'30px'
+  },
+  chip:{
+    float:'left',
+    position: 'relative',
+    top:-80,
+    left:180
+  },
+  myName: {
+    userSelect:'none',
+    textAlign:'left',
+    position:'relative',
+    top:'-40px',
+    left:'-190px'
+  },
 })
 
 
 class Author extends React.Component {
+  state = {
+    current:'',
+    fans:[]
+  }
+  componentDidMount () {
+    let current = window.location.pathname.split('/')[2]
+    current && this.setState({ current })
+    // 获取我的信息
+    this.props.getAuthorInfo()
+  }
+  componentWillReceiveProps (nextProps, nextState) {
+    if (nextProps.author) {
+      this.setState({ fans:nextProps.author.fans })
+    }
+  }
   render () {
-    const { classes } = this.props
-    return <div className={classes.root}>
-      <Grid container spacing={0}>
-        <Grid className={classes.root} item xs={4}>
-          <div className='sidebar'>
-            <div style={{ textAlign:'center', borderBottom:'1px solid #ccc' }}>
-              <Avatar className={classes.avatar} src={projectConfig.myVatar} />
-              <p>{projectConfig.des}</p>
-            </div>
-            <List component='nav'>
-              <ListItem style={{ textAlign:'center' }} button>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText>
-                  <span style={{ color:'#fff' }}>作者信息</span></ListItemText>
-              </ListItem>
-              <ListItem style={{ textAlign:'center' }} button>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText style={{ color:'#fff' }}>
-                  <span style={{ color:'#fff' }}>个人分享</span>
-                </ListItemText>
-              </ListItem>
-              <ListItem style={{ textAlign:'center' }} button>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText style={{ color:'#fff' }}>
-                  <span style={{ color:'#fff' }}>留言板</span>
-                </ListItemText>
-              </ListItem>
-            </List>
+    const { classes, match } = this.props
+    const { current, fans } = this.state
+    return <div>
+      <div className="sidebar"/>
+      <div>
+        <Paper className={classes.paper}>
+          <div className={classes.navContainer}>
+            <Link to="/author/aboutme">
+              <Tooltip title="关于作者" placement="right">
+                <Fab
+                  onClick={() => this.routeChange('aboutme')}
+                  className={current === 'aboutme'? classes.currentMenu :classes.fabButton}
+                  color="inherit" aria-label="Add" variant="extended">
+                  <AccountCircleOutlined style={iconStyle} />
+                </Fab>
+              </Tooltip>
+            </Link>
+            <Link to="/author/message">
+              <Tooltip title="留言板" placement="right">
+                <Fab
+                  onClick={() => this.routeChange('message') }
+                  className={current === 'message' ? classes.currentMenu : classes.fabButton}
+                  color="inherit" aria-label="Add" variant="extended">
+                  <ChatBubbleOutline style={iconStyle}/>
+                </Fab>
+              </Tooltip>
+            </Link>
+            <Link to="/author/share">
+              <Tooltip title="个人分享" placement="right">
+                <Fab
+                  onClick={() => this.routeChange('share') }
+                  className={current === 'share' ? classes.currentMenu : classes.fabButton} color="inherit" aria-label="Add" variant="extended">
+                  <ShareOutlined style={iconStyle} />
+                </Fab>
+              </Tooltip>
+            </Link>
           </div>
-        </Grid>
-        <Grid item xs={2}></Grid>
-        <Grid item xs={6}>
-        </Grid>
-      </Grid>
+          <Avatar className={classes.avatar} src={projectConfig.myAvatar} />
+          <div className={classes.chip}>
+            <Chip color="primary" variant="outlined" label={projectConfig.des} icon={<AlarmOn/>} />
+          </div>
+          <div className={classes.myName}>
+            <Typography component="span" variant="h5">
+              樊元甲
+            </Typography>
+          </div>
+          <Likes likesAuthor={this.props.likesAuthor} likes={fans} />
+          {this.props.children}
+        </Paper>
+      </div>
     </div>
+  }
+  routeChange = (current) => {
+    this.setState({ current })
   }
 }
 
